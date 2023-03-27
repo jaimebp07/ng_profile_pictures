735b7edb68589e65ee83a60d50bfce2dda722216
jbaron5bdb <jbaron5@bancodebogota.com.co>
2023-03-26 21:02:47 -0500

    ci: this is the first test of the pipeline in this project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('profile_picture');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('profile_picture app is running!');
-  });
 });
